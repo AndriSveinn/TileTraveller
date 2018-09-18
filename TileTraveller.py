218d0c36a985e546e7f8f38afd6a2620386aaf0d
Andri Sveinn <andrii17@ru.is>
2018-09-18 09:10:57 +0000

    first commit

---
+++ TileTraveller.py
@@ -0,0 +1,14 @@
+Staðsetning = [1,1]
+norður='(N)orth' 
+suður='(S)outh'
+austur='(W)est'
+vestur='(E)ast'
+
+
+while position != [3,1]:
+    if position = [1,1]:
+        print('You can travel:' + norður + '.')
+        færsla=input('Direction:')
+        færsla=færsla.lower()
+        while færlsa != 'n':
+            print('Not a valid direction')
